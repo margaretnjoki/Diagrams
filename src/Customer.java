1a0ba0cfe5fd1d24890946445687a4fae34e60f8
margaretnjoki <maggykiroko@gmail.com>
2026-07-01 08:56:25 +0300

    reading diagrams and writing the code

---
+++ src/Customer.java
@@ -0,0 +1,23 @@
+public class Customer {
+    private String name;
+    private String address;
+    private String email;
+
+    public Customer(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
